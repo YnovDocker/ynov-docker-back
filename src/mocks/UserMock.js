623b357008c335d5f5243c52755ab1ed6feefc3b
amaitreynov <antoine.maitre@ynov.com>
2017-04-12 01:52:23 +0200

    [User][tests] Modified mock for UserController

--- src/mocks/UserMock.js
+++ src/mocks/UserMock.js
@@ -1,51 +1,44 @@
 const faker = require('faker');
 
-const User = {
-		username: faker.name.findName(),
-		email: faker.internet.email(),
-		password: faker.random,
-		firstname: faker.name.findName(),
-		lastname: faker.name.findName(),
-		role: faker.random
+module.exports = {
+		User: {
+				username: faker.name.findName(),
+				email: faker.internet.email(),
+				password: faker.random,
+				firstname: faker.name.findName(),
+				lastname: faker.name.findName(),
+				role: faker.random
+		},
+		RegisterUserObject: {
+				username: "username",
+				email: "username@test.com",
+				password: "password",
+				firstname: "firstname",
+				lastname: "lastname",
+				passwordConfirmation: "password"
+		},
+		RegisterUserObjectBadPwConfirmation: {
+				username: "username",
+				email: "username@test.com",
+				password: "password",
+				firstname: "firstname",
+				lastname: "lastname",
+				passwordConfirmation: "wrongPassword"
+		},
+		RegisterUserObjectBadEmail: {
+				username: "username",
+				email: "username@test.",
+				password: "password",
+				firstname: "firstname",
+				lastname: "lastname",
+				passwordConfirmation: "password"
+		},
+		RegisterUserObjectEmptyField: {
+				username: "username",
+				email: "username@test.",
+				password: "password",
+				firstname: "",
+				lastname: "",
+				passwordConfirmation: "password"
+		}
 };
-module.exports = User;
-
-const RegisterUserObject = {
-		username: "username",
-		email: "username@test.com",
-		password: "password",
-		firstname: "firstname",
-		lastname: "lastname",
-		passwordConfirmation: "password"
-};
-module.exports = RegisterUserObject;
-
-const RegisterUserObjectBadPwConfirmation = {
-		username: "username",
-		email: "username@test.com",
-		password: "password",
-		firstname: "firstname",
-		lastname: "lastname",
-		passwordConfirmation: "wrongPassword"
-};
-module.exports = RegisterUserObjectBadPwConfirmation;
-
-const RegisterUserObjectBadEmail = {
-		username: "username",
-		email: "username@test.",
-		password: "password",
-		firstname: "firstname",
-		lastname: "lastname",
-		passwordConfirmation: "password"
-};
-module.exports = RegisterUserObjectBadEmail;
-
-const RegisterUserObjectEmptyField = {
-		username: "username",
-		email: "username@test.",
-		password: "password",
-		firstname: "",
-		lastname: "",
-		passwordConfirmation: "password"
-};
-module.exports = RegisterUserObjectEmptyField;
