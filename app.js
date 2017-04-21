d182d4733c72fa77a9812cf196a7459e0d823e7d
amaitreynov <antoine.maitre@ynov.com>
2017-04-21 21:43:49 +0200

    [CORS][auth] Fixed CORS related problems - front updated aswell
    - cors: allow origin set to *
    - auth: refactored res.200 with .end(JSON.stringify(jsonObj) format

--- app.js
+++ app.js
@@ -3,33 +3,34 @@
 'use strict';
 //library
 const swaggerExpress = require('swagger-express-mw'),
-    swaggerTools = require('swagger-tools'),
-    path = require('path'),
-    yaml = require('js-yaml'),
-    fs = require('fs'),
-    jsonfile = require('jsonfile'),
-    config = require('config'),
-    log4js = require('log4js'),
-    mongoose = require('mongoose'),
-    multer = require('multer');
+		swaggerTools = require('swagger-tools'),
+		path = require('path'),
+		cors = require('cors'),
+		yaml = require('js-yaml'),
+		fs = require('fs'),
+		jsonfile = require('jsonfile'),
+		config = require('config'),
+		log4js = require('log4js'),
+		mongoose = require('mongoose'),
+		multer = require('multer');
 
 
 mongoose.Promise = Promise;
 
 let logger = log4js.getLogger('server.core'),
-    token = require('./src/helpers/token');
+		token = require('./src/helpers/token');
 
 //config variable
 const swaggerSpecFilePath = __dirname + '/api/swagger.json';
 const port = process.env.PORT || 10010;
 
 function allowCORS(req, res, next) {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Credentials", true);
-    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
-    res.set("Access-Control-Expose-Headers", "Content-Type, token");
-    res.set("Access-Control-Allow-Methods", "GET, POST, PUT, HEAD, DELETE, OPTIONS");
-    next();
+		res.set("Access-Control-Allow-Origin", "*");
+		res.set("Access-Control-Allow-Credentials", true);
+		res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+		res.set("Access-Control-Expose-Headers", "Content-Type, token");
+		res.set("Access-Control-Allow-Methods", "GET, POST, PUT, HEAD, DELETE, OPTIONS");
+		next();
 }
 
 //app
@@ -40,115 +41,120 @@ logger.info('/*************** STARTING SERVER ***************/');
 logger.info('/*************** *************** ***************/');
 
 try {
-    const configFile = yaml.safeLoad(fs.readFileSync(__dirname + '/api/swagger.yaml', 'utf8'));
-    const indentedJson = JSON.stringify(configFile, null, 4);
-    //console.log(indentedJson);
-    fs.writeFile(swaggerSpecFilePath, indentedJson, function (err) {
-        if (err) {
-            throw err;
-        }
-        logger.info('swagger.json write for /docs');
+		const configFile = yaml.safeLoad(fs.readFileSync(__dirname + '/api/swagger.yaml', 'utf8'));
+		const indentedJson = JSON.stringify(configFile, null, 4);
+		//console.log(indentedJson);
+		fs.writeFile(swaggerSpecFilePath, indentedJson, function (err) {
+				if (err) {
+						throw err;
+				}
+				logger.info('swagger.json write for /docs');
 
-        //initialize swagger.json
-        let swaggerDoc = require(swaggerSpecFilePath);
-        swaggerDoc.host = config.server.host + ':' + port;
+				//initialize swagger.json
+				let swaggerDoc = require(swaggerSpecFilePath);
+				swaggerDoc.host = config.server.host + ':' + port;
 
+				//using CORS
+				logger.info('Using CORS');
+				app.options('*', cors());
 
-        module.exports = app; // for testing
+				module.exports = app; // for testing
 
-        const swaggerConfig = {
-            appRoot: __dirname, // required config
-            swaggerFile: __dirname + config.AppPaths.swaggerFile,
-            controllersDir: __dirname + config.AppPaths.controllersDir,
-            mockControllersDir: __dirname + config.AppPaths.mockControllersDir,
-            helpers: __dirname + config.AppPaths.helpers,
-            controllers: __dirname + config.AppPaths.controllers,
-            useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
-        };
+				const swaggerConfig = {
+						appRoot: __dirname, // required config
+						swaggerFile: __dirname + config.AppPaths.swaggerFile,
+						controllersDir: __dirname + config.AppPaths.controllersDir,
+						mockControllersDir: __dirname + config.AppPaths.mockControllersDir,
+						helpers: __dirname + config.AppPaths.helpers,
+						controllers: __dirname + config.AppPaths.controllers,
+						useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+				};
 
-        // Initialize the Swagger middleware
-        swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+				// Initialize the Swagger middleware
+				swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
-            app.use(log4js.connectLogger(log4js.getLogger('server.http'), {level: log4js.levels.INFO}));
+						app.use(log4js.connectLogger(log4js.getLogger('server.http'), {level: log4js.levels.INFO}));
 
-            app.use(allowCORS);
-            //connect to the mongo DB
-            const mongoUrl = config.server.mongo.connectionString;
-            const mongoOpts = config.server.mongo.options;
+						app.use(allowCORS);
 
-            let isConnectedBefore = false;
-            let connectWithRetry = function () {
-                return mongoose.connect(mongoUrl, mongoOpts, function (err) {
-                    if (err) {
-                        logger.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
-                        setTimeout(connectWithRetry, 5000);
-                    }
-                });
-            };
+						//connect to the mongo DB
+						const mongoUrl = config.server.mongo.connectionString;
+						const mongoOpts = config.server.mongo.options;
 
-            connectWithRetry();
+						let isConnectedBefore = false;
+						let connectWithRetry = function () {
+								return mongoose.connect(mongoUrl, mongoOpts, function (err) {
+										if (err) {
+												logger.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+												setTimeout(connectWithRetry, 5000);
+										}
+								});
+						};
 
-            mongoose.connection.on('error', function () {
-                logger.error('Could not connect to MongoDB');
-            });
+						connectWithRetry();
 
-            mongoose.connection.on('disconnected', function () {
-                logger.warn('Lost MongoDB connection...');
-                if (!isConnectedBefore)
-                    connectWithRetry();
-            });
+						mongoose.connection.on('error', function () {
+								logger.error('Could not connect to MongoDB');
+						});
 
-            mongoose.connection.on('connected', function () {
-                isConnectedBefore = true;
-                logger.info('Connection established to MongoDB');
-                logger.info('/*************** SERVER STARTED ***************/');
-            });
+						mongoose.connection.on('disconnected', function () {
+								logger.warn('Lost MongoDB connection...');
+								if (!isConnectedBefore) {
+										connectWithRetry();
+								}
+						});
 
-            mongoose.connection.on('reconnected', function () {
-                logger.info('Reconnected to MongoDB');
-            });
+						mongoose.connection.on('connected', function () {
+								isConnectedBefore = true;
+								logger.info('Connection established to MongoDB');
+								logger.info('/*************** SERVER STARTED ***************/');
+						});
 
-            // Close the Mongoose connection, when receiving SIGINT
-            process.on('SIGINT', function () {
-                logger.warn('Process received SIGINT signal - going to exit ...');
-                mongoose.connection.close(function () {
-                    logger.warn('Force to close the MongoDB connection');
-                    process.exit(0);
-                });
-            });
+						mongoose.connection.on('reconnected', function () {
+								logger.info('Reconnected to MongoDB');
+						});
 
-            logger.info('Using token handler middleware');
-            //token.initialize();
-            //app.use(token.tokenHandler);
+						// Close the Mongoose connection, when receiving SIGINT
+						process.on('SIGINT', function () {
+								logger.warn('Process received SIGINT signal - going to exit ...');
+								mongoose.connection.close(function () {
+										logger.warn('Force to close the MongoDB connection');
+										process.exit(0);
+								});
+						});
 
-            // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-            app.use(middleware.swaggerMetadata());
+						logger.info('Using token handler middleware');
+						//token.initialize();
+						//app.use(token.tokenHandler);
 
-            // Validate Swagger requests
-            app.use(middleware.swaggerValidator());
+						// Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+						app.use(middleware.swaggerMetadata());
 
-            // Route validated requests to appropriate controller
-            app.use(middleware.swaggerRouter(swaggerConfig));
+						// Validate Swagger requests
+						app.use(middleware.swaggerValidator());
 
-            // Serve the Swagger documents and Swagger UI
-            app.use(middleware.swaggerUi());
+						// Route validated requests to appropriate controller
+						app.use(middleware.swaggerRouter(swaggerConfig));
 
-            // Start the server
-            swaggerExpress.create(swaggerConfig, function (err, swaggerExpress) {
-                if (err) {
-                    throw err;
-                }
+						// Serve the Swagger documents and Swagger UI
+						app.use(middleware.swaggerUi());
 
-                // install middleware
-                swaggerExpress.register(app);
+						// Start the server
+						swaggerExpress.create(swaggerConfig, function (err, swaggerExpress) {
+								if (err) {
+										throw err;
+								}
 
-                app.listen(port);
-                logger.info('Your server is listening on port %d (http://%s:%d)', port, config.server.host, port);
-            });
-        });
-    });
+								// install middleware
+								swaggerExpress.register(app);
+
+								app.listen(port);
+								logger.info('Your server is listening on port %d (http://%s:%d)', port, config.server.host, port);
+						});
+				});
+		});
 } catch (e) {
-    console.log(e);
+		console.log(e);
 }
 
 
--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,80 +6,81 @@
 
 'use strict';
 let util = require('util'),
-    mongoose = require('mongoose'),
-    logger = require('log4js').getLogger('controller.auth'),
-    _ = require('lodash'),
-    token = require('../helpers/token'),
-    UserDB = require('../models/User'),
-    User = mongoose.model('User');
+		mongoose = require('mongoose'),
+		logger = require('log4js').getLogger('controller.auth'),
+		_ = require('lodash'),
+		token = require('../helpers/token'),
+		UserDB = require('../models/User'),
+		User = mongoose.model('User');
 
 module.exports = {
-    auth: auth
+		auth: auth
 };
 
 function auth(req, res, next) {
-    logger.info('Authenticate user with login: ' + req.swagger.params.authObject.value.username);
+		logger.info('Authenticate user with login: ' + req.swagger.params.authObject.value.username);
 
-    let username = req.swagger.params.authObject.value.username;
-    let password = req.swagger.params.authObject.value.password;
+		let username = req.swagger.params.authObject.value.username;
+		let password = req.swagger.params.authObject.value.password;
 
-    User.getAuthenticated(username, password,
-        function (err, user, reason) {
-            if (err) {
-                logger.error(err);
-                return next(err);
-            }
+		User.getAuthenticated(username, password,
+				function (err, user, reason) {
+						if (err) {
+								logger.error(err);
+								return next(err);
+						}
 
-            if (_.isNull(user) || _.isEmpty(user)) {
-                res.set('Content-Type', 'application/json');
-                res.status(401).json({
-                        error: {
-                            errorCode: 'E_INVALID_CREDENTIALS',
-                            errorMessage: 'Invalid username or password.'
-                        }
-                    } || {}, null, 2);
-            }
-            else {
-                logger.info('Creating user token for: ', user.username);
-                let tokenObject = token.createBasicToken(user._id, user.username, user.firstname, user.lastname);
-                token.setResponseToken(tokenObject, res,
-                    function (tokenCrypted) {
-                        let authResponse = {
-                            userId: tokenObject.userId,
-                            username: tokenObject.username,
-                            firstname: tokenObject.firstname,
-                            lastname: tokenObject.lastname,
-                            expirationDate: tokenObject.expirationDate
-                        };
-                        logger.info('token: ' + JSON.stringify(tokenObject));
-                        //res.json(authResponse);
-                        // logger.info('authResponse object created: \n' + JSON.stringify(authResponse));
-                        // logger.info('user object created: \n' + user);
-                        User.findOneAndUpdate(
-                            {_id: authResponse.userId},
-                            {
-                                $set: {
-                                    connectionToken: tokenCrypted,
-                                }
-                            },
-                            {new: true}, //means we want the DB to return the updated document instead of the old one
-                            function (err, updatedUser) {
-                                if (err)
-                                    return next(err);
-                                else {
-                                    res.set('Content-Type', 'application/json');
-                                    res.status(200).json({
-                                            success: {
-                                                successCode: 'USER_AUTH',
-                                                successMessage: 'User authenticated',
-                                                tokenObject: authResponse,
-                                                token: tokenCrypted
-                                            }
-                                        } || {}, null, 2);
-                                }
-                            });
-                    });
-            }
-        });
+						if (_.isNull(user) || _.isEmpty(user)) {
+								res.set('Content-Type', 'application/json');
+								res.status(401).json({
+												error: {
+														errorCode: 'E_INVALID_CREDENTIALS',
+														errorMessage: 'Invalid username or password.'
+												}
+										} || {}, null, 2);
+						}
+						else {
+								logger.info('Creating user token for: ', user.username);
+								let tokenObject = token.createBasicToken(user._id, user.username, user.firstname, user.lastname);
+								token.setResponseToken(tokenObject, res,
+										function (tokenCrypted) {
+												let authResponse = {
+														userId: tokenObject.userId,
+														username: tokenObject.username,
+														firstname: tokenObject.firstname,
+														lastname: tokenObject.lastname,
+														expirationDate: tokenObject.expirationDate
+												};
+												logger.info('token: ' + JSON.stringify(tokenObject));
+												//res.json(authResponse);
+												// logger.info('authResponse object created: \n' + JSON.stringify(authResponse));
+												// logger.info('user object created: \n' + user);
+												User.findOneAndUpdate(
+														{_id: authResponse.userId},
+														{
+																$set: {
+																		connectionToken: tokenCrypted,
+																}
+														},
+														{new: true}, //means we want the DB to return the updated document instead of the old one
+														function (err, updatedUser) {
+																if (err) {
+																		return next(err);
+																}
+																else {
+																		res.set('Content-Type', 'application/json');
+																		res.status(200).end(JSON.stringify({
+																						success: {
+																								successCode: 'USER_AUTH',
+																								successMessage: 'User authenticated',
+																								tokenObject: authResponse,
+																								token: tokenCrypted
+																						}
+																				} || {}, null, 2));
+																}
+														});
+										});
+						}
+				});
 }
 
